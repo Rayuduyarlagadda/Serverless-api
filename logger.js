d03c15f865a391e755f1cb247d28e88e3bc19fb3
Rayuduyarlagadda <urytrayudu1@gmail.com>
2022-01-02 23:19:47 +0530

    checking in logger implementation

---
+++ logger.js
@@ -0,0 +1,40 @@
+var winston = require('winston'),
+CloudWatchTransport = require('winston-cloudwatch');
+
+var NODE_ENV = process.env.NODE_ENV || 'development';
+
+const logger = new winston.Logger({
+  transports: [
+    new (winston.transports.Console)({
+      timestamp: true,
+      colorize: true,
+    })
+  ]
+});
+
+var config = {
+  logGroupName: 'my-log-group',
+  logStreamName: NODE_ENV,
+  createLogGroup: false,
+  createLogStream: true,
+  awsConfig: {
+    accessKeyId: process.env.CLOUDWATCH_ACCESS_KEY_ID,
+    secretAccessKey: process.env.CLOUDWATCH_SECRET_ACCESS_KEY,
+    region: process.env.CLOUDWATCH_REGION
+  },
+  formatLog: function (item) {
+    return item.level + ': ' + item.message + ' ' + JSON.stringify(item.meta)
+  }
+}
+
+if (NODE_ENV != 'development') logger.add(CloudWatchTransport, config);
+
+logger.level = process.env.LOG_LEVEL || "silly";
+
+logger.stream = {
+  write: function(message, encoding) {
+    logger.info(message);
+  }
+};
+
+module.exports = logger;
